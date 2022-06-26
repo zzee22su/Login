a0fcd435ae6529afa2b7b4dece63bd7b59fe7176
zzee22su <patrickstar0910@gmail.com>
2022-06-26 21:40:46 +0900

    ADD : 부트스트랩을 이용하여 기본 로그인/패스워드 창 틀 갖추기

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  chainWebpack: config => {
-    config.module.rules.delete('eslint');
-  }
+  transpileDependencies: true
 })
