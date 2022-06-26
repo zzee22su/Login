233d0ecaf5f1150394e6ef703475f2b9d29eeb57
zzee22su <patrickstar0910@gmail.com>
2022-06-26 21:31:59 +0900

    FIX : eslint-disable-next-line

---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <!-- <div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword">
+    </div>
+  </div> -->
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'login-compo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
